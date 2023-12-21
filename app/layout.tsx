b85f22d079dd763c0837768f420dc9c21c3a8606
Felipe <felipeeduardo817@gmail.com>
2023-12-20 23:29:30 -0300

    atualizando responsividade

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={nunitoSans.className}>
-        <main className='bg-gray-100 min-h-screen  flex flex-col items-center'>
+        <main className='bg-gray-100 min-h-screen flex flex-col items-center'>
           <nav className='w-full bg-white h-16 flex items-center justify-center'>
-            <section className='container flex items-center gap-3'>
+            <section className='container flex items-center gap-3 pl-2'>
 
               <Link href={"/"}>
               <Image src="/logo.svg" width={48} height={48} alt='imagem do logo' className='cursor-pointer'  />
--- app/pais/[name]/page.tsx
+++ app/pais/[name]/page.tsx
@@ -38,15 +38,15 @@ export default async function CountryPage({ params: { name } }: { params: {name:
 
   return (
 
-  <section className="flex flex-col container">
+  <section className="flex flex-col container p-5 sm:p-0">
 
   <h1 className="text-5xl text-center font-bold text-gray-800 my-16">{country.translations.por.common}</h1>
 
-    <Link className="flex items-center py-2 gap-1 cursor-pointer" href="/">
+    <Link className="flex items-center py-2 pl-2 gap-1 cursor-pointer" href="/">
       
       <Image src="/arrow-back.svg" alt="icone de voltar" width={15} height={15} />Voltar </Link>
 
-    <article className="flex md:flex-row flex-col justify-between min-w-full p-10 bg-white rounded-xl">
+    <article className="flex md:flex-row flex-col justify-between min-w-full p-5 sm:p-10 bg-white rounded-xl gap-1">
 
       <section>
 
@@ -82,14 +82,14 @@ export default async function CountryPage({ params: { name } }: { params: {name:
 
       </section>
       
-      <div className="relative h-48 my-2 md:h-auto w-96 shadow-md md:order-last order-first">
+      <div className="relative h-48 my-2 md:h-auto sm:w-96 fill shadow-md md:order-last order-first">
         <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover"/>
       </div>
 
 
     </article>
     <section>
-      <h3 className="mt-12 text-2xl font-semibold text-gray-800">Países que fazem fronteira</h3>
+      <h3 className="mt-12 text-2xl font-semibold text-gray-800 pb-3">Países que fazem fronteira</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2">
         {borderCountries?.map((border, i) => (
         <CountryCard key={"fronteira" + i} {... border}/> ) )}
